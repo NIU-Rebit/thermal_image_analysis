9cbfbf4c4964a6fc8bad0c31b31cccd76958e946
LaZoark <emersonchen086@icloud.com>
2021-12-17 22:59:01 +0800

    normal version

--- grid_cv.py
+++ grid_cv.py
@@ -1,14 +1,14 @@
 import numpy as np
 import cv2 
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(3)
 while True:
     if cap.isOpened():
         ret, image = cap.read()
         height, width, channels = image.shape   # 先取得影像尺寸
         break
         # print(height, width, channels)
-cols, rows = (width, height)
+cols, rows = (width*2, height*2)
 
 # image = np.zeros((rows, cols, 3), dtype = "uint8")
 step = 20       # grid的密度
@@ -21,19 +21,18 @@ cv2.namedWindow('grid', 0)
 
 while cap.isOpened():
     ret, image = cap.read()
-
+    image = cv2.resize(image, (width, height))
     for i in range(step):
         v_xy.append( [int(x[i]), 0, int(x[i]), rows-1] )
         h_xy.append( [0, int(y[i]), cols-1, int(y[i])] )
         [x1, y1, x2, y2] = v_xy[i]
         [x1_, y1_, x2_, y2_] = h_xy[i]
-        cv2.line(image, (x1,y1), (x2, y2), (255,0,255), thickness)       # vertical
-        cv2.line(image, (x1_,y1_), (x2_, y2_), (255,0,255), thickness)   # horizontal
+        cv2.line(image, (x1,y1), (x2, y2), (255,255,0), thickness)       # vertical
+        cv2.line(image, (x1_,y1_), (x2_, y2_), (255,255,0), thickness)   # horizontal
         cv2.imshow('grid', image)
-        # cv2.waitKey(50) # 觀察grid掃蕩
+        # cv2.waitKey(10) # 觀察grid掃蕩
     if cv2.waitKey(1) == ord('q'):
        break
 
-# cv2.waitKey(0)
 cv2.destroyAllWindows()
 cap.release()
